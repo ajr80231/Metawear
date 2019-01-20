515d9d01e4b7a89c6548e60b2141d2ed6de89b1e
ajr80231 <ajr80231@gmail.com>
2019-01-20 01:48:06 -0500

    Initial Commit

---
+++ Muscle Fatigue/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Muscle Fatigue
+//
+//  Created by Antonio Rodriguez on 1/20/19.
+//  Copyright © 2019 uga. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
